e212cd0245a6977d24b8cb2299142d71b4618034
admin <mmd83@63.ru>
2023-07-06 07:19:51 +0400

    Reports

--- src/main/java/ru/skyprolessons/spring/HomeWork1Spring/service/ReportServiceImpl.java
+++ src/main/java/ru/skyprolessons/spring/HomeWork1Spring/service/ReportServiceImpl.java
@@ -2,12 +2,15 @@ package ru.skyprolessons.spring.HomeWork1Spring.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.skyprolessons.spring.HomeWork1Spring.pojo.Report;
 import ru.skyprolessons.spring.HomeWork1Spring.repository.ReportRepository;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,9 +20,13 @@ public class ReportServiceImpl implements ReportService {
     private final ReportRepository reportRepository;
 
     private static ObjectMapper objectMapper = new ObjectMapper();
+
+    @Value("${path.fileforreports}")
+    String pathFileForReports;
+//    private File fileForReports = new File(new URI(pathFileForReports));
     private File fileForReports = new File("src/main/java/ru/skyprolessons/spring/homework1spring/files/json.txt");
 
-    public ReportServiceImpl(ReportRepository reportRepository) {
+    public ReportServiceImpl(ReportRepository reportRepository) throws URISyntaxException {
         this.reportRepository = reportRepository;
     }
 
